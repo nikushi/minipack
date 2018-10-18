c718b31aef79184ed4114b9607e24883a9bfa599
Nobuhiro Nikushi <deneb.ge@gmail.com>
2018-10-18 20:49:56 +0900

    Add more test for helpers

--- spec/webpack_manifest/rails/helper_spec.rb
+++ spec/webpack_manifest/rails/helper_spec.rb
@@ -78,6 +78,23 @@ RSpec.describe WebpackManifest::Rails::Helper do
       end
     end
 
+    context 'with multiple manifests registration and with manifest: option' do
+      subject { helper.javascript_bundle_tag('admin-application', manifest: :admin) }
+
+      let(:manifest_admin_path) { File.expand_path('../../support/files/manifest-admin.json', __dir__) }
+      let(:configuration) do
+        WebpackManifest::Rails::Configuration.new.tap do |c|
+          c.cache = false
+          c.add :shop, manifest_path
+          c.add :admin, manifest_admin_path
+        end
+      end
+
+      it 'renders a nice <script> tag' do
+        is_expected.to eq '<script src="/packs/admin/admin-application-5d7c7164b8a0a9d675fad9ab410eaa8d.js"></script>'
+      end
+    end
+
     context 'given non-existing *.js entry name' do
       subject do
         -> { helper.javascript_bundle_tag('not_found') }
@@ -98,6 +115,23 @@ RSpec.describe WebpackManifest::Rails::Helper do
       end
     end
 
+    context 'with multiple manifests registration and with manifest: option' do
+      subject { helper.stylesheet_bundle_tag('admin-application', manifest: :admin) }
+
+      let(:manifest_admin_path) { File.expand_path('../../support/files/manifest-admin.json', __dir__) }
+      let(:configuration) do
+        WebpackManifest::Rails::Configuration.new.tap do |c|
+          c.cache = false
+          c.add :shop, manifest_path
+          c.add :admin, manifest_admin_path
+        end
+      end
+
+      it 'renders a nice <link> tag' do
+        is_expected.to eq '<link rel="stylesheet" media="screen" href="/packs/admin/admin-application-5d7c7164b8a0a9d675fad9ab410eaa8d.css" />'
+      end
+    end
+
     context 'given non-existing *.css entry name' do
       subject do
         -> { helper.stylesheet_bundle_tag('not_found') }
@@ -115,6 +149,23 @@ RSpec.describe WebpackManifest::Rails::Helper do
 
       it 'renders a nice <img> tag' do
         is_expected.to eq '<img src="/packs/union-ok-857e5bfa272e71b6384046f68ba29d44.png" />'
+      end
+    end
+
+    context 'with multiple manifests registration and with manifest: option' do
+      subject { helper.image_bundle_tag('admin-icon.png', manifest: :admin) }
+
+      let(:manifest_admin_path) { File.expand_path('../../support/files/manifest-admin.json', __dir__) }
+      let(:configuration) do
+        WebpackManifest::Rails::Configuration.new.tap do |c|
+          c.cache = false
+          c.add :shop, manifest_path
+          c.add :admin, manifest_admin_path
+        end
+      end
+
+      it 'renders a nice <img> tag' do
+        is_expected.to eq '<img src="/packs/admin/admin-favicon-5d7c7164b8a0a9d675fad9ab410eaa8d.png" />'
       end
     end
 

6fc56698ab523fe92b07162ea6e76789987d1be8
Nobuhiro Nikushi <deneb.ge@gmail.com>
2019-05-05 23:48:30 +0900

    Fix initializer label

--- lib/webpack_manifest/rails/railtie.rb
+++ lib/webpack_manifest/rails/railtie.rb
@@ -5,7 +5,7 @@ require 'rails'
 module WebpackManifest
   module Rails
     class Railtie < ::Rails::Railtie
-      initializer "webpack_manifest.set_root_path" do
+      initializer "webpack_manifest.set_defaults" do
         WebpackManifest::Rails.configuration.root_path = ::Rails.root.to_s
         WebpackManifest::Rails.configuration.base_path = ::Rails.root.to_s
       end

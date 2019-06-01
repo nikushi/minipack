23df01581bf8d3b5c62fda0acdea6c18b941a4b3
Nobuhiro Nikushi <deneb.ge@gmail.com>
2019-06-01 23:51:13 +0900

    Revert "Plugin loader (#17)"
    This reverts commit 078b6e83c4dce3366de9ff9d79663b67036e007f.

--- lib/minipack.rb
+++ lib/minipack.rb
@@ -22,49 +22,13 @@ module Minipack
       @configuration
     end
     attr_writer :configuration
-
-    # Find all 'minipack_plugin' files in $LOAD_PATH and load them
-    def load_env_plugins
-
-      files = []
-      glob = '**/minipack_plugin.rb'
-      $LOAD_PATH.each do |load_path|
-        globbed = Dir.glob(File.expand_path(glob, load_path))
-
-        globbed.each do |load_path_file|
-          files << load_path_file if File.file?(load_path_file)
-        end
-      end
-
-      load_plugin_files files
-    end
-
-    def load_plugins
-      load_plugin_files ::Gem.find_latest_files('minipack/**/minipack_plugin', false)
-    end
-
-    private
-
-    def load_plugin_files(plugins)
-      plugins.each do |plugin|
-        begin
-          load plugin
-        rescue ::Exception => e
-          details = "#{plugin.inspect}: #{e.message} (#{e.class})"
-          warn "Error loading Minipack plugin #{details}"
-        end
-      end
-    end
   end
 end
-
-# To keep backward compatibility
-require_relative 'webpack_manifest'
 
 require 'active_support/lazy_load_hooks'
 ActiveSupport.on_load :action_view do
   ::ActionView::Base.send :include, Minipack::Helper
 end
 
-Minipack.load_env_plugins
-Minipack.load_plugins
+# To keep backward compatibility
+require_relative 'webpack_manifest'
